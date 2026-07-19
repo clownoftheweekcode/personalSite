fb111a50129970b56c2e30b0065ab2031756f7c4
clownoftheweekcode <clownofthe_week.code@outlook.com>
2026-07-19 21:58:33 +0000

    added a few images, added content to most if not all tabs.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let mainEl = document.getElementById("main");
 let homeBtn = document.getElementById("home"), projectsBtn = document.getElementById("projects"), artworkBtn = document.getElementById("artwork"), favoritesBtn = document.getElementById("favorites"), otherBtn = document.getElementById("other");
+let allNavBtn = document.querySelectorAll("navBtn");
+
 homeBtn.addEventListener("click", function() {
     mainEl.innerHTML = `<span class = "header">
                         <h4 id = "aboutHeader">ABOUT</h4>
@@ -25,21 +27,46 @@ projectsBtn.addEventListener("click", function() {
                                 <p>webOS</p>
                             </a>
                         </span>
-                        <span id = "Yahtzee"></span>
+                        <span id = "yahtzeeWrap">
+                        <span id = "Yahtzee">
+                            <a onclick = 'window.location.href = "https://github.com/clownoftheweekcode/yahtzee-game.git"'>
+                                <img class = "projectThumb" src = "./images/projectYahtzeeThumb.png">
+                                <p>yahtzee</p>
+                            </a>
+                        </span>
+                        </span>
                         <span id = "macroPad"></span>
                     </p>
                     `;
 });
 
 artworkBtn.addEventListener("click", function() {
-    mainEl.innerHTML = ``;
+    mainEl.innerHTML = `
+        <h3 class = "header">ARTWORKS</h3>
+        <img width = "150px" src = "./images/catGuy (1).webp">
+        <img width = "220px" src = "./images/OneOkRockContestEntry (1).webp">
+    `;
 });
 
 favoritesBtn.addEventListener("click", function() {
-    mainEl.innerHTML = ``;
+    mainEl.innerHTML = `
+    <h3 class = "header">FAVORITE THINGS</h3>
+    <p>I am in many different fandoms, a few are JJBA, Hannibal, Dexter, Dr.Stone, tokyo ghoul, TGCF, and MANY more!</p>
+    <p>my favorite characters in the few i listed are:</p>
+    <ul>
+    <li>Bruno Bucciarati</li>
+    <li>Joseph Joestar</li>
+    <li>Shishio Tsukasa</li>
+    <li>Stanley Snyder</li>
+    <li>Seidou Takizawa</li>
+    <li>Hua Cheng</li>
+    </ul>
+    `;
 });
 
 otherBtn.addEventListener("click", function() {
-    mainEl.innerHTML = ``;
+    mainEl.innerHTML = `
+        <p>nothing yet...</p>
+    `;
 });
 
